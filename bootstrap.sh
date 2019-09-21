c7880c551958d547e7c6da411ca6dbebbe4eff53
Adem Budak <admbudk@gmail.com>
2019-09-21 15:24:27 +0300

    Polish with some comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # abort process if any of them fail
+
 su -c
 apt-get install sudo 
 
@@ -9,15 +11,18 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install build-essential git yadm curl wget keepassx ninja-build cmake libpython-dev libpython3-dev
-yadm clone https://github.com/p1v0t/dotfiles.git
 
+## install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 rm get-pip.py
 
+## build vim 
 
+### clone
 git clone --depth=1 https://github.com/vim/vim.git; cd vim
 
+### configure
 sudo ./configure --enable-fail-if-missing \
 --disable-darwin \
 --disable-smack \
@@ -42,21 +47,28 @@ sudo ./configure --enable-fail-if-missing \
 --disable-gtktest \
 --with-compiledby=p1v0t
 
+### make and install
 sudo make -j 8
 sudo make install
 
-# vim-plug
+## get plugin manager 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# swith to fish
+### clone configuration files
+yadm clone https://github.com/p1v0t/dotfiles.git
+
+## install fish shell
+###  Add repository source
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' > /etc/apt/sources.list.d/shells:fish:release:3.list
 sudo apt-get update
 sudo apt-get install fish
 
+### install with package manager
 wget -nv https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key -O Release.key
 sudo apt-key add - < Release.key
 sudo apt-get update
 
+### change current shell with fish
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
